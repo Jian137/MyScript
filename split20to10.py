4a14dbe059a21f3c8e29f97b6c1ee44814e18ccf
Jian137 <hj1301700049@gmail.com>
2022-01-06 00:34:57 +0800

    change

--- split20to10.py
+++ split20to10.py
@@ -3,20 +3,22 @@ import shutil
 from tqdm import tqdm
 from glob import glob
 
-FileFolderPath = "/data/hj/mmediting-master/hlm2_track3/*"
-SavePath = "/data/hj/mmediting-master/hlm_track3_7/"
+FileFolderPath = "/data/hj/mmediting-master/rzwc/*"
+SavePath = "/data/hj/mmediting-master/wzt_7/"
 FileFolders = sorted(glob(FileFolderPath))
-
+num = 7
+if not os.path.exists(SavePath):
+    os.mkdir(SavePath)
 if __name__=="__main__":
     i = 0
     for n,FileFolder in enumerate(tqdm(FileFolders)):
         Files = sorted(glob(os.path.join(FileFolder,"*.png")))
         for m,File in enumerate(Files):
-            if m%7 == 0:
+            if m%num == 0:
                 i=i+1
                 TmpPath = os.path.join(SavePath,str(i).zfill(4))
                 if not os.path.exists(TmpPath):
                     os.mkdir(TmpPath)
-                shutil.copy(File,os.path.join(TmpPath,str(m%7).zfill(8)+".png"))
+                shutil.copy(File,os.path.join(TmpPath,str(m%num).zfill(8)+".png"))
             else :
-                shutil.copy(File,os.path.join(TmpPath,str(m%7).zfill(8)+".png"))      
+                shutil.copy(File,os.path.join(TmpPath,str(m%num).zfill(8)+".png"))      
